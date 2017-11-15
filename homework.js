ad2f10dc91df718ca730529dbd327193240f29ee
yuanmin564 <yuanmin564@163.com>
2017-11-15 21:21:12 +0800

    11月15日float样式和图表修改

--- homework.js
+++ homework.js
@@ -1,28 +1,28 @@
-// 引入 ECharts 主模块
-var echarts = require('echarts/lib/echarts');
-// 引入柱状图
-require('echarts/lib/chart/line');
-// 引入提示框和标题组件
-require('echarts/lib/component/tooltip');
-require('echarts/lib/component/title');
 //初始化图表
-var myChart = echarts.init(document.getElementById('chart'));
-var options = {
-    lengend: {
-        data: '单日实收金额',
-    },
-    xAxis: {
-        type: 'category',
-        data: ['2017-11-10', '2017-11-11', '2017-11-12', '2017-11-13', '2017-11-14'],
-        axisTick: false,
-    },
-    yAxis: {
-        type: 'value',
-        axisTick: false,
-    },
-    series: [
-        type: 'line',
-        name: '单日实收金额',
-        data: [600,400,700,500,300,],
-};
-myChart.setOption(options);
+console.dir(echarts)
+window.onload =  function () {
+    var id = document.getElementById('myChart')
+    var myChart = echarts.init(document.getElementById('myChart'));
+    var options = {
+        title: {
+            //text: 'ECharts 入门示例'
+        },
+        tooltip: {},
+        legend: {
+            data: ['单日实收金额'],
+            left: '5%'
+        },
+        xAxis: {
+            data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+
+        },
+        yAxis: {},
+        series: [{
+            name: '单日实收金额',
+            type: 'line',
+            data: [5, 20, 36, 10, 10, 20]
+        }]
+    };
+
+    myChart.setOption(options);
+}
