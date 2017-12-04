19a77d748c74f72838e48e7e8fcba6c05eb03ac0
yuanmin564 <yuanmin564@163.com>
2017-12-04 22:39:10 +0800

    12月1日至4日，优化表格样式

--- homework.js
+++ homework.js
@@ -5,26 +5,52 @@ window.onload =  function () {
     var myChart = echarts.init(document.getElementById('myChart'));
     var colors = ['#5793f3', '#d14a61', '#675bba'];
     var options = {
-        title: {
-
-        },
+        title: {},
         tooltip: {},
         legend: {
             data: ['单日实收金额'],
             left: '5%'
         },
         xAxis: {
-            data: ["20171110","20171111","20171112","20171113","20171114","20171115","20171116"],
+            data: ["20171110", "20171111", "20171112", "20171113", "20171114", "20171115", "20171116"],
             nameLocation: 'end',
+            nameGap: 0,
+            boundaryGap: false,
+            axisLine: {
+                show: false,
+            },
+            axisTick: {
+                lineStyle: {
+                    color: '#e4e5ea',
+                },
+            },
+            splitLine: {
+                show: true,
+            }
         },
-        yAxis: {},
+        yAxis: {
+            type: 'value',
+            nameLocation: 'start',
+            axisLine: {
+                show: false,
+            },
+            axisTick: {
+                lineStyle: {
+                    color: '#e4e5ea',
+                },
+            },
+        },
         series: [{
             name: '单日实收金额',
             type: 'line',
-            data: [200, 240, 360, 410, 310, 820,760],
+            data: [200, 240, 360, 410, 310, 820, 760],
             smooth: true,
+            itemStyle: {
+                normal: {
+                    color: '#1fd55e',
+                }
+            }
         }]
     };
-
     myChart.setOption(options);
 }
